03b34cec291ae04d79b73feadb6f4ce58bdb468a
jayeola <jayeola@gmail.com>
2016-06-02 14:02:28 +0100

    added Gemfile. new tests for docker

--- spec/jenkins/jenkins_spec.rb
+++ spec/jenkins/jenkins_spec.rb
@@ -21,10 +21,7 @@ describe service('FirewallD') do
   it { should be_enabled }
   it { should be_running }
 end
-# describe port(80) do
-#   it { should be_listening }
-# end
-#
+
 describe port(8080) do
   it { should be_listening }
 end
